ac3a8ddbfb22ef44d8d28fe00c222be876e4f0ff
Evgenii Sopov <mrseakg@gmail.com>
2016-09-13 13:52:11 +0700

    Init first

---
+++ www/index.php
@@ -0,0 +1,80 @@
+<?php
+
+if(isset($_POST['addlog']) && isset($_POST['fname'])){
+	$filename = $_POST['fname'];
+	
+	file_put_contents('logs/'.$filename, $_POST['addlog']);
+	header('Location: ?file='.$filename);
+	exit();
+}
+
+if(isset($_POST['deletefile'])){
+	$filename = $_POST['deletefile'];
+	unlink('logs/'.$filename);
+	header('Location: ?');
+	exit();
+}
+
+
+$fname = 'log_'.date('Ymd_His', time()).'.log';
+echo "
+	  <button href='javascript:void(0);' onclick='addtext.style.display=\"block\"'>Add Log</button>
+
+<form method='POST' id='addtext' style='display: none'>
+	<input type=text name='fname' size=50 value='".$fname."'/><br>
+	<textarea name=addlog cols=50 rows=15></textarea><br><br>
+	<input type=submit value='save'/>
+	<input type=button href='javascript:void(0);' onclick='addtext.style.display=\"none\"' value='Cancel'/>
+</form>
+<hr>";
+
+$files  = scandir("logs/");
+
+foreach($files as $i => $fname){
+	if(preg_match("/.*\\.log/i", $fname)) {
+		echo "<button onclick='window.location.href=\"?file=".$fname."\"'>".$fname."</button>";
+	}
+}
+
+echo "<hr> ";
+
+echo "
+	<style>
+		p { margin: 0px; }
+		p.unknown { color: black; }
+		p.weblog { color: lightblue; }
+		p.mediaplayersdk { color: gray; margin-left: 50px; }
+		p.playerevents { color: black; font-weight: bold; margin-left: 25px; }
+	</style>
+";
+
+function getClass($line){
+	if(preg_match("/\\[LOG\\].*/i", $line)) {
+		return "weblog";
+	}
+	
+	if(preg_match("/\\d+:\\d+:\\d+:(player\\.c|ffmpeg_content_provider\\.c|content_provider_thread\\.c|video_renderer_thread\\.c|egl_video_renderer_provider\\.c|video_decoder_thread\\.c|recorder_provider_thread\\.c).*/i", $line)){
+		return "mediaplayersdk";
+	}
+	if(preg_match("/^(PLP_|CP_).*/i", $line)){
+		return "playerevents";
+	}
+	
+	return "unknown";
+}
+
+if(isset($_GET['file'])){
+	$filename = $_GET['file'];
+	$handle = fopen('logs/'.$filename, "r");
+	if ($handle) {
+		echo "<h1>".$filename;
+		echo "  <form method='POST' style='display: inline-block' ><input type=hidden name='deletefile' value='".$filename."'/><input type=submit value='delete log'/></form></h1>";
+		
+		while (($line = fgets($handle)) !== false) {
+			echo "<p class='".getClass($line)."'>".$line."</p>";
+		}
+		fclose($handle);
+	} else {
+		echo "Could not open file: ".$filename."<br>";
+	} 
+}
